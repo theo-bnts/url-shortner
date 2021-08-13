95a7f353da03c54970715410d2461d6d7a656893
Théo Bontemps <55360392+theo-bnts@users.noreply.github.com>
2021-08-13 16:58:07 +0200

    full rewrite

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -2,36 +2,35 @@ addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 
+
+
 async function handleRequest(request) {
-    const requestUrl = new URL(request.url)
-    const path = requestUrl.pathname.replace('/s/', '')
-    if (path === ADMIN_TOKEN) {
-        let data
-        try {
-            data = await request.json()
-        } catch (e) {
-            return new Response(
-                JSON.stringify({
-                    success: false,
-                    error: 'invalid body'
-                }), {
-                    status: 400,
-                    headers: new Headers({
-                        'content-type': 'application/json'
-                    })
-                }
-            )
-        }
-        return await addRedirection(data, requestUrl)
-    } else
-        return await redirect(path)
+    const {pathname, origin} = new URL(request.url)
+
+    const shortCode = pathname
+        .replace('/r', '')
+        .replace('/', '')
+
+    if (shortCode === ADMIN_TOKEN)
+        return await handleAdminRequest(request, origin)
+    else
+        return await redirect(shortCode, pathname.startsWith('/r'))
 }
 
-async function redirect(shortName) {
-    const url = await db.get(shortName)
-    if (url)
-        return Response.redirect(url)
-    else
+
+
+async function redirect(shortCode, urlRewrite) {
+    const url = await db.get(shortCode)
+
+    if (url) {
+
+        if (urlRewrite)
+            return Response.redirect(url)
+        else
+            return await fetch(url)
+            
+    } else {
+
         return new Response(
             JSON.stringify({
                 success: false,
@@ -43,28 +42,47 @@ async function redirect(shortName) {
                 })
             }
         )
+    }
 }
 
-async function addRedirection(data, requestUrl) {
-    if (data.shortCode && data.target) {
-        const existingUrl = await db.get(data.shortCode)
-        if (!existingUrl) {
-            db.put(data.shortCode, data.target)
+
+
+async function handleAdminRequest(request, urlOrigin) {
+    
+        var data
+
+        try {
+            data = await request.json()
+        } catch (e) {
+
             return new Response(
                 JSON.stringify({
-                    success: true,
-                    data: {
-                        shortUrl: requestUrl.origin + '/s/' + data.shortCode,
-                        target: data.target
-                    }
+                    success: false,
+                    error: 'invalid body'
                 }), {
-                    status: 200,
+                    status: 400,
                     headers: new Headers({
                         'content-type': 'application/json'
                     })
                 }
             )
-        } else
+
+        }
+
+        if (!data.shortCode || !data.target)
+            return new Response(
+                JSON.stringify({
+                    success: false,
+                    error: 'missing parameters'
+                }), {
+                    status: 422,
+                    headers: new Headers({
+                        'content-type': 'application/json'
+                    })
+                }
+            )
+
+        if (await db.get(data.shortCode))
             return new Response(
                 JSON.stringify({
                     success: false,
@@ -76,16 +94,28 @@ async function addRedirection(data, requestUrl) {
                     })
                 }
             )
-    } else
-        return new Response(
-            JSON.stringify({
-                success: false,
-                error: 'missing parameters'
-            }), {
-                status: 422,
-                headers: new Headers({
-                    'content-type': 'application/json'
-                })
+
+        return await addRedirection(data, urlOrigin)
+}
+
+
+
+async function addRedirection(data, urlOrigin) {
+
+    db.put(data.shortCode, data.target)
+
+    return new Response(
+        JSON.stringify({
+            success: true,
+            data: {
+                shortUrl: urlOrigin + '/' + data.shortCode,
+                target: data.target
             }
-        )
+        }), {
+            status: 200,
+            headers: new Headers({
+                'content-type': 'application/json'
+            })
+        }
+    )
 }
